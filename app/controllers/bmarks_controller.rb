92e4fef388d510e555e191ddfca2221596bc464d
Natalie Kueneman <tabalie@gmail.com>
2016-01-24 23:39:35 -0700

    Working on bmark controller.

--- app/controllers/bmarks_controller.rb
+++ app/controllers/bmarks_controller.rb
@@ -8,8 +8,20 @@ class BmarksController < ApplicationController
   end
 
   def new
+    @bmark = Bmark.new
   end
 
-  def edit
+def create
+  @bmark = Bmark.new(params.require(:bmark).permit(:title, :body))
+    if @bmark.save
+      flash[:notice] = "beautymark was saved."
+      redirect_to @bmark
+    else
+      flash[:error] = "There was an error saving this beautymark. Please try again."
+      render :new
+    end
+end
+
+def edit
   end
 end
